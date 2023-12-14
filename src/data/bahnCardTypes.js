1e78c39eed707abd3aea19b7156b5508b79ae35e
Paul <paul.ramisch@gmail.com>
2023-12-14 11:23:26 +0100

    Update Bahncard-Preise

--- src/data/bahnCardTypes.js
+++ src/data/bahnCardTypes.js
@@ -4,28 +4,28 @@ const bahnCards = {
         "ageGroup": "6-18",
         "ticketClass": 2,
         "discount": 0.25,
-        "price": 12.00
+        "price": 7.90
     },
     "mBC25_2": {
         "name": "My BahnCard 25",
         "ageGroup": "19-26",
         "ticketClass": 2,
         "discount": 0.25,
-        "price": 36.90
+        "price": 39.90
     },
     "mBC25_1": {
         "name": "My BahnCard 25",
         "ageGroup": "19-26",
         "ticketClass": 1,
         "discount": 0.25,
-        "price": 77.90
+        "price": 81.10
     },
     "mBC50_2": {
         "name": "My BahnCard 50",
         "ageGroup": "19-26",
         "ticketClass": 2,
         "discount": 0.50,
-        "price": 69.90
+        "price": 79.90
     },
     "mBC50_1": {
         "name": "My BahnCard 50",
@@ -39,14 +39,14 @@ const bahnCards = {
         "ageGroup": "27-64",
         "ticketClass": 2,
         "discount": 0.25,
-        "price": 59.90
+        "price": 62.90
     },
     "BC25_1": {
         "name": "BahnCard 25",
         "ageGroup": "27-64",
         "ticketClass": 1,
         "discount": 0.25,
-        "price": 121.00
+        "price": 125.00
     },
     "BC50_2": {
         "name": "BahnCard 50",
@@ -67,28 +67,28 @@ const bahnCards = {
         "ageGroup": "27-64",
         "ticketClass": 2,
         "discount": 1,
-        "price": 4339.00
+        "price": 4550.00
     },
     "BC100_1": {
         "name": "BahnCard 100",
         "ageGroup": "27-64",
         "ticketClass": 1,
         "discount": 1,
-        "price": 7356.00
+        "price": 7714.00
     },
     "sBC25_2": {
         "name": "Senioren BahnCard 25",
         "ageGroup": "65+",
         "ticketClass": 2,
         "discount": 0.25,
-        "price": 38.90
+        "price": 40.90
     },
     "sBC25_1": {
         "name": "Senioren BahnCard 25",
         "ageGroup": "65+",
         "ticketClass": 1,
         "discount": 0.25,
-        "price": 77.90
+        "price": 81.90
     },
     "sBC50_2": {
         "name": "Senioren BahnCard 50",
